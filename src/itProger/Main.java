6ad3e17493567a906a3ffa318450bfaf4e237301
Volodymyr Petrunin <ktoya@gmail.com>
2023-01-02 15:18:51 +0100

    Add branch main

---
+++ src/itProger/Main.java
@@ -0,0 +1,9 @@
+package itProger;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("git add ." + "git commit -m 'Add file' " + "git branch newBranch" + "git checkout newBranch"
+                        + "Теперь создаём папку blog  с файлами: index.js, index.html" + "git checkout main"
+                + "git branch -M main" + "git remote add origin https://github.com/VolodymyrPetrunin/HomeWork.git"
+                + "git push -u origin main");
+    }
+}

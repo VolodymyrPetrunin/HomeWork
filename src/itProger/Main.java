ec1cf8320710b711caed0292fc7dc20fac1acef0
Volodymyr Petrunin <ktoya@gmail.com>
2023-01-02 15:34:43 +0100

    Update

--- src/itProger/Main.java
+++ src/itProger/Main.java
@@ -2,7 +2,7 @@ package itProger;
 public class Main {
     public static void main(String[] args) {
         System.out.println("git add ." + "git commit -m 'Add file' " + "git branch newBranch" + "git checkout newBranch"
-                        + "Теперь создаём папку blog  с файлами: index.js, index.html" + "git checkout main"
+                        + "Теперь создаём папку blog  с файлами: index.js, index.html" + "git add ." + "git commit -m 'Add new file' " +  "git checkout main"
                 + "git branch -M main" + "git remote add origin https://github.com/VolodymyrPetrunin/HomeWork.git"
                 + "git push -u origin main");
     }
